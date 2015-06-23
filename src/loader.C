8c55ec2fd48221c0528b782349e22373563a581a
souvik1982 <souvik1982@gmail.com>
2015-06-22 21:01:13 -0500

    loader

---
+++ src/loader.C
@@ -0,0 +1,6 @@
+#include <vector>
+// #ifdef __MAKECINT__
+#pragma link C++ class vector<vector<float> >+;
+#pragma link C++ class vector<vector<unsigned> >+;
+#pragma link C++ class vector<vector<vector<unsigned> > >+;
+// #endif
